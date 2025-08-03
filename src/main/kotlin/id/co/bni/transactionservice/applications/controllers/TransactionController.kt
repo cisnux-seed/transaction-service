db9ead41fa708411ac707b2ae9bae7d175596743
cisnux <fajrarisqulla@gmail.com>
2025-08-03 07:40:56 +0700

    feat: resource

--- src/main/kotlin/id/co/bni/transactionservice/applications/controllers/TransactionController.kt
+++ src/main/kotlin/id/co/bni/transactionservice/applications/controllers/TransactionController.kt
@@ -11,15 +11,15 @@ import kotlinx.coroutines.slf4j.MDCContext
 import kotlinx.coroutines.withContext
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
-@Controller
+@RestController
 @RequestMapping("/api/transaction")
 class TransactionController(
     private val transactionService: HistoricalTrxService
